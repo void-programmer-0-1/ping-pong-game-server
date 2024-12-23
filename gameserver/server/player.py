85003611686277e9f04116d76376b3bfb43ca41a
santosh <santoshmohanramk1383@gmail.com>
2024-12-23 10:10:52 +0530

    paddle initialization started

--- gameserver/server/player.py
+++ gameserver/server/player.py
@@ -11,13 +11,17 @@ class Player:
         self.is_winner: bool = False
         self.is_player_active: bool = is_player_active
     
-    def set_paddles(self,canvas_config: CanvasConfig):
-        self.player_paddle.set_paddle_height(100)
-        self.player_paddle.set_paddle_width(15)
-        self.player_paddle.set_paddle_x(15)
-        self.player_paddle.set_paddle_y(canvas_config.screen_height / 2 - self.player_paddle.paddle_height / 2)
-        self.player_paddle.set_paddle_velocity_x(5)
-        self.player_paddle.is_player_ai(False)
+    def set_paddles(self,canvas_config: CanvasConfig, side: str):
+        if side == "left":
+            self.player_paddle.set_paddle_height(100)
+            self.player_paddle.set_paddle_width(15)
+            self.player_paddle.set_paddle_x(15)
+            self.player_paddle.set_paddle_y(canvas_config.screen_height / 2 - self.player_paddle.paddle_height / 2)
+            self.player_paddle.set_paddle_velocity_x(5)
+            self.player_paddle.is_player_ai(False)
+
+        else:
+            ...
 
     def update_player_paddle(self):
         pass
